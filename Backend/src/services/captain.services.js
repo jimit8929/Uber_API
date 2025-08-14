8f347327c233a63d3229ce32e03b437668b63500
jimit8929 <jimit8929@gmail.com>
2025-08-14 18:03:54 +0530

    Changes Updated

--- Backend/src/services/captain.services.js
+++ Backend/src/services/captain.services.js
@@ -2,7 +2,7 @@ import captainModel from "../models/captain.model.js"
 
 export const createCaptain = async({firstname , lastname , email , password,  color , plate , vehicleType , capacity}) => {
 
-  console.log(firstname, lastname, email, password, color, plate, vehicleType, capacity);
+  // console.log(firstname, lastname, email, password, color, plate, vehicleType, capacity);
 
   if(!firstname || !lastname || !email || !password || !color || !plate || !vehicleType   ||!capacity){
     return res.status(400).json({message:"All fields are required"});
@@ -25,7 +25,7 @@ export const createCaptain = async({firstname , lastname , email , password,  co
     }
   });
 
-  console.log(captain);
+  // console.log(captain);
 
   return captain;
 }
